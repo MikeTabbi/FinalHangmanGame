812fadfcbd799c0716caa6860efeb801f8f84bf8
mddanquahtabbi21students.desu.edu <mdanquahtabbi@gmail.com>
2023-04-17 14:01:28 -0400

    Initial Commit

---
+++ src/App.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+ class ButtonExample {
+    public static void main(String[] args) {
+
+        // Creates a new frame and button
+        JFrame f =new JFrame("Hangman Menu");
+        JButton b =new JButton("Start Game");
+
+        // Sets the color of the words in button
+        b.setForeground(Color.BLACK);
+
+        // X, Y, Width, and Height of button
+        b.setBounds(660,300,125,40);
+
+        // The menu screen image
+        JLabel hangmanImage = CustomTools.loadImage(CommonConstants.PACMAN_IMAGE);
+        hangmanImage.setBounds(0, 0, hangmanImage.getPreferredSize().width, hangmanImage.getPreferredSize().height);
+
+
+
+        // When the button is clicked the game starts with ActionListener
+        b.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                new Hangman().setVisible(true);
+            }
+        });
+        f.add(b);
+        f.setSize(1450,840);
+        f.setLayout(null);
+        f.setVisible(true);
+        f.getContentPane().add(hangmanImage);
+
+    }
+}
+
